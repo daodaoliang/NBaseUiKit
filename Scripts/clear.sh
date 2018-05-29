33de66f4a493a4299849ea091ee1957740f5c8f2
daodaoliang <daodaoliang@yeah.net>
2018-05-29 11:06:08 +0800

    add clear script

---
+++ Scripts/clear.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf ..\NBaseUiKit\release
+rm -rf ..\NBaseUiKit\debug
+rm -rf ..\NBaseUiKit\build_
+rm -rf \Example\build_
+rm -rf ..\Example\release
+rm -rf ..\Example\debug
+rm -rf ..\bin
+rm -rf ..\Makefile*
+rm -rf ..\NBaseUiKit\Makefile*
+rm -rf ..\Example\Makefile*
